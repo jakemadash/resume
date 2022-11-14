fcfb622547bc87b4da2a1acc6d88d76879243a2e
Jake Ashton <jakemaddenashton@gmail.com>
2022-11-13 20:26:26 -0700

    Create basic file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: { number: 1, text: "", id: uniqid() },
+      tasks: [],
+    };
+  }
+  render() {
+    return ()
+  }
+}
+
+export default App;
