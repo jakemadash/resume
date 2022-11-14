849c337bd1eefab5e48080bf82d41d5da9e50689
Jake Ashton <jakemaddenashton@gmail.com>
2022-11-13 22:26:42 -0700

    Write basic form input component

---
+++ src/components/Input.js
@@ -0,0 +1,44 @@
+import React from "react";
+import uniqid from "uniqid";
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      id: uniqid(),
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+      id: this.state.id,
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: "",
+      id: uniqid(),
+    });
+  };
+
+  render() {
+    const { value } = this.state.value;
+    return (
+      <div>
+        <label htmlFor={this.props.label}></label>
+        <input
+          onChange={this.handleChange}
+          value={value}
+          type={this.props.type}
+          id={this.props.label}
+        />
+      </div>
+    );
+  }
+}
+
+export default Input;
