0771bfdf78a143603c444810e9a2e5cae49292db
Jake Ashton <jakemaddenashton@gmail.com>
2022-11-16 15:31:00 -0700

    Clean up files, write logic to add new forms for multiple schools

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import Personal from "./components/Personal";
 import Education from "./components/Education";
+import EducationAdd from "./components/EducationAdd";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      schools: [<Education />],
+    };
+  }
+  add = (e) => {
+    e.preventDefault();
+    console.log(e);
+    if (e.target.classList.contains("education")) {
+      const newSchool = <Education />;
+      this.setState({
+        schools: this.state.schools.concat(newSchool),
+      });
+    }
+  };
   render() {
     return (
-      <div>
+      <div className="app">
         <Personal />
-        <Education />;
+        <div className="education">
+          <h1>Education</h1>
+          {this.state.schools}
+        </div>
+        <button className="education" onClick={this.add}>
+          Add
+        </button>
       </div>
     );
   }
--- src/components/Education.js
+++ src/components/Education.js
@@ -4,8 +4,6 @@ import Input from "./Input";
 class Education extends React.Component {
   render() {
     return (
-      <div className="section">
-        <h1>Education</h1>
         <form>
           <Input label="institution" type="text" />
           <Input label="degree" type="text" />
@@ -13,7 +11,6 @@ class Education extends React.Component {
           <Input label="from" type="date" />
           <Input label="to" type="date" />
         </form>
-      </div>
     );
   }
 }
